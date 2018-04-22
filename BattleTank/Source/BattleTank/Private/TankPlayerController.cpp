42c9e05530ebdcce30804b53dd9db0f537aa6f2c
RYAN-PC\ryan <rnsmcity@gmail.com>
2018-04-22 13:48:47 -0400

    Adding tank & Tank player controller blueprints. Improve grass to appear more seamless.

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+#include "Tank.h"
+
+ATank * ATankPlayerController::GetControlledTank() const {
+
+	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Error, TEXT("PlayerController not possessing a tank"));
+	} else {
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *(ControlledTank->GetName()));
+	}
+
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController BeginPlay()"));
+}
